e4cf546a7a58dd75ddb83b03fa6d27d2627e249c
Azhar Uddin <akhan@gainsight.com>
2019-08-04 23:40:00 +0530

    Added Stack class

---
+++ algorithms /stack.js
@@ -0,0 +1,18 @@
+class Stack {
+    constructor() {
+        this.items = {};
+        this.first = null;
+        this.last = null;
+    }
+    push() {
+
+    }
+    pop() {
+
+    }
+    print() {
+
+    }
+}
+
+var stack = new Stack();
